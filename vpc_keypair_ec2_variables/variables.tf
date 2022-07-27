4953915d740c9db4196e9373ecad250b29a33bd3
busani9 <31929250+busani9@users.noreply.github.com>
2022-07-27 07:59:16 +0530

    Update variables.tf

--- vpc_keypair_ec2_variables/variables.tf
+++ vpc_keypair_ec2_variables/variables.tf
@@ -13,3 +13,16 @@ default = "ap-south-1b"
 variable subnet2_cidr_block {
 default = "192.168.2.0/24"
 }
+
+
+variable ami-id {
+default = "ami-08df646e18b182346"
+}
+
+variable keypair {
+default = "dev"
+}
+
+variable instance_type {
+default = "t2.micro"
+}
